371ec41df89c76d9e2ecafa5de7dc7916b414463
ChristoferMendes <christoferluizdsm@gmail.com>
2023-04-20 13:43:28 -0300

    new .env variable

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,8 +1,9 @@
 import { app } from "./app";
 import { routes } from "./shared/routes";
-
+import { config } from "dotenv";
+config()
 
 app.use(routes)
 
 const PORT = 4000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(process.env.PORT || PORT, () => console.log(`Server is running on port ${PORT}`))
