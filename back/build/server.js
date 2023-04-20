5c1a313fe088d37697c89477ad96111ac4c70094
ChristoferMendes <christoferluizdsm@gmail.com>
2023-04-20 13:51:16 -0300

    new scripts

--- back/build/server.js
+++ back/build/server.js
@@ -2,6 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
 const routes_1 = require("./shared/routes");
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
 app_1.app.use(routes_1.routes);
-const PORT = 4500;
-app_1.app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+const PORT = 4000;
+app_1.app.listen(process.env.PORT || PORT, () => console.log(`Server is running on port ${PORT}`));
