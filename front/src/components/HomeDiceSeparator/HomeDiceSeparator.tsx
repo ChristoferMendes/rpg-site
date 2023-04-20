5deb302fd009700a17aa1e3c69cda0cb3e84a9ea
ChristoferMendes <christoferluizdsm@gmail.com>
2023-04-20 11:26:12 -0300

    added dice from figma

--- front/src/components/HomeDiceSeparator/HomeDiceSeparator.tsx
+++ front/src/components/HomeDiceSeparator/HomeDiceSeparator.tsx
@@ -1,16 +1,8 @@
-import { GiDiceFire } from "react-icons/gi";
-
 export function HomeDiceSeparator() {
   return (
     <div className='flex justify-between items-center mx-2'>
       <div className='w-40 h-[1px] bg-purple-900' />
-      <svg width="0" height="0">
-        <linearGradient id="blue-gradient" x1="100%" y1="100%" x2="0%" y2="0%">
-          <stop stopColor="#5146b9" offset="0%" />
-          <stop stopColor="#591885" offset="100%" />
-        </linearGradient>
-      </svg>
-      <GiDiceFire style={{ fill: "url(#blue-gradient)" }} className='text-5xl' />
+      <img src="/dice.svg" alt="" />
       <div className='w-40 h-[1px] bg-purple-900' />
     </div>
   );
