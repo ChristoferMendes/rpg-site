57a1edfe81bbc9e715e599dd39ff968c940ca723
ChristoferMendes <christoferluizdsm@gmail.com>
2023-04-20 13:36:02 -0300

    hello world route

--- back/src/shared/routes.ts
+++ back/src/shared/routes.ts
@@ -4,5 +4,6 @@ import { characterRouter } from "../modules/characters/routes";
 
 export const routes = Router();
 
+routes.use('/', (_, res) => res.send('Hello World!'))
 routes.use('/users', userRouter)
 routes.use('/characters', characterRouter)
